9b64e35bba171e00f68e0e48406f78b51609b532
xihe <im0k@outlook.com>
2023-11-03 22:31:11 +0800

    init Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: ['~/assets/scss/index.scss'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+  devtools: { enabled: false },
+  modules: ["@nuxtjs/tailwindcss"],
 });
